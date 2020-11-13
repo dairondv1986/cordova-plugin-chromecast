365e0430c068232cc8cba150b242b86d575f0bf5
Dairon <dairondv1986@gmail.com>
2020-11-13 12:00:12 -0300

    Update MLPCastUtilities.m
    Restoring back to startAbsoluteTime (after some tests...) calculation can't be equal startTime in buffered content because the Chromecast use the startTime for seeking the content when needs start at some reproduction point and relativeStartTime for the calculation of the start time showed in Chromecast's time bar.

--- src/ios/MLPCastUtilities.m
+++ src/ios/MLPCastUtilities.m
@@ -18,13 +18,15 @@ NSDictionary* queueOrderIDsByItemId = nil;
     mediaInfoBuilder.streamDuration = duration;
     if ([streamType isEqualToString:@"buffered"]) {
         mediaInfoBuilder.streamType = GCKMediaStreamTypeBuffered;
+        mediaInfoBuilder.startAbsoluteTime = 0;
     } else if ([streamType isEqualToString:@"live"]) {
         mediaInfoBuilder.streamType = GCKMediaStreamTypeLive;        
+        mediaInfoBuilder.startAbsoluteTime = startTime;
     } else {
         mediaInfoBuilder.streamType = GCKMediaStreamTypeNone;
+        mediaInfoBuilder.startAbsoluteTime = startTime;
     }    
-    
-    mediaInfoBuilder.startAbsoluteTime = startTime;    
+            
     mediaInfoBuilder.metadata = [MLPCastUtilities buildMediaMetadata:metaData];
     mediaInfoBuilder.textTrackStyle = [MLPCastUtilities buildTextTrackStyle:textTrackStyle];
 
